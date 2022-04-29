7ee745fc7f9c652e2f7e2aeee092f84d8d759be0
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-29 10:20:01 -0300

    First commit

---
+++ src/test/java/com/samucatezu/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samucatezu.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
